cb2f5cb099aa1c2f801d00801672de964dcc4de4
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2025-01-28 14:57:02 +1300

    Update apical.js

--- apical.js
+++ apical.js
@@ -1,13 +1,13 @@
-function apiCall() {
-  const xhttpr = new XMLHttpRequest();
-  xhttpr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+
+const JSONDiv = document.getElementById("jsondata");
+const xhttpr = new XMLHttpRequest();
+xhttpr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
   
-  xhttpr.send();
+xhttpr.send();
   
-  xhttpr.onload = ()=> {
-    if (xhttpr.status === 200) {
-        const response = JSON.parse(xhttpr.response);
-        alert(response);
-    }
-  };
-}
+xhttpr.onload = ()=> {
+  if (xhttpr.status === 200) {
+      const response = JSON.parse(xhttpr.response);
+      JSONDiv.innerhtml = response;
+  }
+};
